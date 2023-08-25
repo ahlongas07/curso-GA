d1794d76c4585095b7ff01ee4b5ce97f0efa3025
ahlongas07 <ahlongas07@gmail.com>
2023-08-25 10:46:41 -0500

    print variable

--- holamundo.py
+++ holamundo.py
@@ -2,7 +2,7 @@ import os
 def main():
     print("Hola Mundo")
     username = os.getenv("USERNAME")
-    print("Hola," + {username}+ "es tu nombre de GitHub")
+    print("Hola," + username + "es tu nombre de GitHub")
 
 if __name__== "__main__":
     main()
