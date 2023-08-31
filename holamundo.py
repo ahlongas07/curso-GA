33fbd93815a9ab66ca6d412d4c2b25ea74be044d
ahlongas07 <ahlongas07@gmail.com>
2023-08-31 10:50:00 -0500

    Chages on holamundo.py

--- holamundo.py
+++ holamundo.py
@@ -1,9 +1,10 @@
 import os
+
 def main():
     print("Hola Mundo")
     username = os.getenv("USERNAME")
     print("Hola, " + username + " es tu nombre de GitHub")
-    print("Esta sería la otra forma: "+ os.getenv("USERNAME") + "de imprimir una variable de ambiente")
+    print("Esta sería la otra forma: "+ os.getenv("USERNAME") + " de imprimir una variable de ambiente")
 
 if __name__== "__main__":
     main()
