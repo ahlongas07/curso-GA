413a0164b2a853f01dc808fac13100bb437c6d69
alejandrolongas <alejandro.longas@globant.com>
2023-09-18 14:28:35 -0500

    Create a push to branch

--- holamundo.py
+++ holamundo.py
@@ -3,7 +3,7 @@ import os
 def main():
     print("Hola Mundo")
     username = os.getenv("USERNAME")
-    print("Hola, " + username + " es tu nombre de GitHub")
+    print("Hola, " + username + " es tu nombre de GitHub e!")
     print("Esta sería la otra forma: "+ os.getenv("USERNAME") + " de imprimir una variable de ambiente")
 
 if __name__== "__main__":
